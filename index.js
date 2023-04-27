5949aa96e4384f79555bac4022a3f3a9385c6063
Paras Anand <qtparas@gmail.com>
2023-04-28 01:25:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 9000;
+
+app.use("/", (req, res) => {
+  res.json({ message: "Hello from express app" });
+});
+
+app.listen(port, () => {
+  console.log(`Starting server on port ${port}`);
+});
