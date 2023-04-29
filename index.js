81a4b9c18b1ec4e11cacd3ecade08e3bda5254fb
Paras Anand <qtparas@gmail.com>
2023-04-30 04:38:53 +0530

    added cors

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const app = express();
 
 const port = 80;
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 app.use(express.json());
 
 app.use("/api/markedQuotes", markedQuotesRoutes);
