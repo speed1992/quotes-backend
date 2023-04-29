4f9fa99ea32c4aaf1af406b731b33406b849aafe
Paras Anand <qtparas@gmail.com>
2023-04-30 04:42:38 +0530

    cors fix

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ const app = express();
 
 const port = 80;
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions)); // Use this after the variable declaration
+app.use(cors());
 
 app.use(express.json());
 
